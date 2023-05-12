f1ac6f149f48cc9140d295e3225d5c44623c5676
Harshad Hegde <hegdehb@gmail.com>
2023-05-12 18:08:36 -0500

    removed importlib

--- {{cookiecutter.project_name}}/src/{{cookiecutter.__project_slug}}/__init__.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.__project_slug}}/__init__.py
@@ -1,8 +1,8 @@
 """{{cookiecutter.project_name}} package."""
-import importlib_metadata
+# import importlib_metadata
 
-try:
-    __version__ = importlib_metadata.version(__name__)
-except importlib_metadata.PackageNotFoundError:
-    # package is not installed
-    __version__ = "0.0.0"  # pragma: no cover
+# try:
+#     __version__ = importlib_metadata.version(__name__)
+# except importlib_metadata.PackageNotFoundError:
+#     # package is not installed
+#     __version__ = "0.0.0"  # pragma: no cover
